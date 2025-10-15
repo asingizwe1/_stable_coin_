d364e233c4abfdcd11b588e628064fc1d2117396
asingizwe1 <asingizwe1louis1@gmail.com>
2025-10-15 06:43:57 +0300

    stable coin primary functionality & Parent_contr

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -25,7 +25,8 @@
 
 pragma solidity ^0.8.18;
 
-
+import {ERC20Burnable,ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 /*
  * @title  DecentralizedStableCoin
  * @author Louis Asingizwe
@@ -37,6 +38,47 @@ pragma solidity ^0.8.18;
  * 
  */
 
-contract DecentralizedStableCoin {
+contract DecentralizedStableCoin is ERC20Burnable,Ownable {
+    /////////////////
+    // Constructor //
+    /////////////////
+
+//since ERC20Burnable is also inheriting from ERC20, we need to pass the name and symbol to the ERC20 constructor
+    constructor() ERC20("DecentralizedStableCoin", "DSC") {}
+  error DecentralizedStableCoin_MustBeMoreThanZero();
+   
+   error DecentralizedStableCoin_NotZeroAddress();
+    //////////////////////
+    // External Functions//
+    ////////////////////// 
+
+    function burn(uint256 amount) public override {
+        super.burn(amount);
+    }
+
+    function burnFrom(address account, uint256 amount) public override onlyOwner {
+     uint256 balance = balanceOf(msg.sender);
+     if(_amount<0){
+revert DecentralizedStableCoin_MustBeMoreThanZero();
+     }
+        if (amount < _amount) {
+            revert DecentralisedStableCoin_BurnAmountExceedsBalance();
+        }
+        super.burn (_amount);
+        //super means use that function from the parent contract
+
+     }
+     function mint(address _to, uint256 _amount) external onlyOwner  returns (boolean){
+        if(_to==address(0)){
+            revert DecentralizedStableCoin_NotZeroAddress();
+        }
+        if(_amount <= 0) {
+
+             revert DecentralizedStableCoin_MustBeMorethanZero();
+        }
+        _mint(to, amount);
+        return true;
+    }
+
+  
  
-}
