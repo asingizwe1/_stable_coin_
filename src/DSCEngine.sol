250e46839ea949081d30bf66fab00d9d7d37ec43
asingizwe1 <asingizwe1louis1@gmail.com>
2025-10-17 07:54:59 +0300

    functions required for the engine

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -24,3 +24,57 @@
 // view & pure functions
 
 pragma solidity ^0.8.18;
+
+contract DSCEngine {
+/**
+ * @title  DSCEngine
+ * @author Louis Asingizwe
+ * SYSTEM TO ENSURE 1 TOKEN ==1$ pegged
+ * algorithmic
+ * Our DSC should be over collateralised
+ * -> at no point should the collateral <= DSC 
+ * simiilar ro DAI but with no fees
+ * this is the core that handles all logic of mining 
+ * @notice This is VERY loosely based on the MakerDAO DSS(DAI) system
+ *
+ */
+
+
+function depositCollateralAndMintDsc() external {
+
+
+
+}
+
+
+function redeemCollateralForDsc() external {    
+//set threshold
+
+}
+
+function burnDsc() external {   
+
+}
+
+function redeemCollateralForDsc() external {   
+
+}
+
+
+function redeemCollateral() external {   
+
+//if someone oays back your minted DSC they can have all your collateral
+//this will motivate people to pay back their DSC and always have collateral
+}
+
+function liquidate() external {   
+//removes people's positions as it gets under collateralised to save the protocol
+}
+
+function mint() external {   
+
+}
+function getHealthFactor() external view {
+//show healthy people are
+
+}
