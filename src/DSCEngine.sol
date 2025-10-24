8639da4058955e6f32ec2bfe7deef4162c7836bb
asingizwe1 <asingizwe1louis1@gmail.com>
2025-10-24 08:25:00 +0300

    link-brownie_AggregatorV3Interface-token_value

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -26,7 +26,11 @@
 pragma solidity ^0.8.18;
 import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-contract DSCEngine {
+import {IERC20} from "@oppenzippelin/contracts/token/ERC20/IERC20.sol";
+import {AggregatorV3Interface} from "chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol" ;
+//forge install smartcontractkit/chainlink-brownie-contracts@0.6.1 --no-commit
+
+contract DSCEngine is ReentrancyGuard {
 /**
  * @title  DSCEngine
  * @author Louis Asingizwe
@@ -50,11 +54,15 @@ contract DSCEngine {
 /////////////////////
 error DSCENgine_NeedsMorethanZero();
 error  DSCENgine__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
+error  DSCENgine__NotAllowedToken();
+error  DSCENgine_TransferFailed();
 
 
 /////STATE VARIABLES///////
 /////////////////////
 //refer to chainlink price feed
+PRECISION=1e18;
+uint256 private constant ADDITIONAL_FEED_PRECISION =1e10;
 mapping(address token=>address PriceFeed) private s_priceFeed;//tokenToPriceFeed
 
 DecentralizedStableCoin private immutable i_dsc;
@@ -110,6 +118,31 @@ i_dsc=DecentralizedStableCoin(dscAddress);
 
 /////EXTERNAL FUNCTIONS///////
 /////////////////////
+function getAccountCollateralValue(address user)public view returns(uint256){
+for(uint256 i=0;i<s_collateralTokens.length;i++){
+address token =s_collateralTokens[i];
+uint256 amount = s_collateralDeposited[user][token];
+totalCollateralValueInUsd +=getUsdValue(token*amount);
+return totalCollateralValueInUsd;//getting the collateral value in USD
+
+}//loop through tokens and add up the value usd of the tokens
+
+}
+ 
+function getUsdValue(address token,uint256 amount) public view returns (uint256){
+//we get price feed of  token and multiply by the value
+//we interruct with aggregator v3
+AggregatorV3Interface priceFeed = AggregatorV3Interface(s_priceFeeds(token));//here we get price feed of the token we are talking of
+//we get price by saying priceFeed.latestRoundData();
+{,int256 price,,,}=priceFeed.latestRoundData();
+//1ETH=1000$
+//The returned value from CL will be 1000*1e8 because in documentation it shows 8dp
+return (uint256(price*ADDITIONAL_FEED_PRECISION)*amount)/PRECISION;
+
+
+
+
+}
 
 function depositCollateralAndMintDsc(address tokenCollateralAddress, uint256 amountCollateral) 
     //we firstly need users to select where collateral is from
