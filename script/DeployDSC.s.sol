7659fb9c22713f861908af421b8968f5c318596d
asingizwe1 <asingizwe1louis1@gmail.com>
2025-11-19 23:16:04 +0300

    DSCEng.deploy script_summary

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -6,6 +6,10 @@ import {DSCEngine} from "../src/DSCEngine.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployDSC is Script{
+address[] public tokenAddresses;
+address[] public priceFeedAddresses;
+
+
 function run() external returns(DecentralizedStableCoin,DSCEngine){
 HelperConfig config=new HelperConfig();
 (  address wethUsdPriceFeed;
@@ -14,15 +18,21 @@ HelperConfig config=new HelperConfig();
           address wbtc;
           uint256 deployerKey;
 )=config.activeNetworkConfig();
-
+tokenAddresses=[weth,wbtc];
+priceFeedAddresses=[wethUsdPriceFeed,wbtcUsdpriceFeed];
 
 vm.startBroadcast();
 DecentralizedStableCoin dsc=new DecentralizedStableCoin();
 //below is gonna take alot of parameters because of the constructor
 //its gonna get those files from a helper config
-DSCEngine dscEngine=new DSCEngine();
+DSCEngine dscEngine=new DSCEngine(tokenAddresses,priceFeedAddresses,address(dsc));
 
+//decentralized stable coin is ownable but it needs to be owned by the engine
+//the decentralisedstablecoin contract has a transferOwnership function
+//we call transfer ownership to transfer ownership to the engine
+dsc.transferOwnership(address(dscEngine));
 vm.stopBroadcast();
+return (dsc,dscEngine);
 }
 
 }
